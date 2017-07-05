5873a25090e4e7f8ef2ff400ab89e33ca0b50288
刘智杰 <462546997@qq.com>
2017-07-05 15:23:24 -0400

    sdf

--- introduce/admin.py
+++ introduce/admin.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 from .models import Introduce
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
-class IntroduceAdmin(admin.ModelAdmin):
+
+class IntroduceAdmin(SummernoteModelAdmin):
     model = Introduce
     list_display = ('id', 'title', 'created')
     search_fields = ('title', )
--- introduce/views.py
+++ introduce/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'introduce/index.html')
+    ins = Introduce.objects.first()
+    return render(request, 'introduce/index.html', {'ins': ins})
