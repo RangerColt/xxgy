596817ae40443428f0cee1093fa3b3002a1b48ec
RangerColt <503147973@qq.com>
2017-09-02 20:33:35 +0800

    #20170902

--- project/admin.py
+++ project/admin.py
@@ -1,19 +1,13 @@
 # coding : utf-8
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Project, Article
+from .models import Article
 
 
 # Register your models here.
 
-class ProjectAdmin(SummernoteModelAdmin):
-    list_display = ('pro_type',)
-    list_per_page = 10
-
 class ArticleAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'pro_type', 'author', 'create')
+    list_display = ('title', 'author', 'create')
     list_per_page = 10
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Project, ProjectAdmin)
-
--- project/models.py
+++ project/models.py
@@ -3,29 +3,22 @@ from django.db import models
 
 # Create your models here.
 
-class Project(models.Model):
-    id = models.AutoField(primary_key=True)
-    PROJECT_TYPE =(
-        ('1', '公益图书馆'),
-        ('2', '阅读推广'),
-        ('3', '扶贫助学'),
-        ('4', '儿童安全教育'),
-        ('5', '女童生理卫生')
-    )
-    pro_type = models.CharField(u'项目类型', max_length=16, choices=PROJECT_TYPE) 
-    
-    class Meta:
-        verbose_name_plural = u'项目分类'
-
 class Article(models.Model):
+    PROJECT_TYPE_CHOICES = (
+            ('1', '公益图书馆'),
+            ('2', '阅读推广'),
+            ('3', '扶贫助学'),
+            ('4', '儿童安全教育'),
+            ('5', '女童生理卫生')
+            )
     id = models.AutoField(primary_key=True)
+    type = models.CharField(u'文章类型', max_length=56, choices=PROJECT_TYPE_CHOICES)
     title = models.CharField(u'标题', max_length=128)
     author = models.CharField(u'作者', max_length=16)
-    create = models.DateTimeField(u'创建日期')
-    update = models.DateTimeField(u'更新日期')
+    create = models.DateTimeField(u'创建日期', auto_now=True)
+    update = models.DateTimeField(u'更新日期', auto_now=True)
     content = models.TextField(u'内容')
 
-    pro_type = models.ForeignKey(Project, default='2')
 
     class Meta:
         verbose_name_plural = u'文章'
