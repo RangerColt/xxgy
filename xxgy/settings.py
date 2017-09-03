c7db84bbd6485f1b7d08aa134bff360357aedb4b
Ranger <ranger@localhost.localdomain>
2017-09-03 19:36:41 +0800

    debug

--- xxgy/settings.py
+++ xxgy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bv^79m-i=ot!h6b_q5k83vf4--2dqngg8(0!z2^b5kkegi-t3u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["www.xxgy.org",
                  "127.0.0.1",
